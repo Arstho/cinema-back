41ab7ffbbfb49c7abfa5fd7da2bba232e4dd129b
Islam <khasievislamcloud@gmail.com>
2023-03-20 17:47:34 +0300

    version 1, авторизация на беке

---
+++ routes/auth.router.js
@@ -0,0 +1,11 @@
+const Router = require("express");
+const router = new Router();
+const controller = require("../controllers/auth.controller");
+const authMiddleware = require("../utils/authChek.middleware");
+
+router.post("/registration", controller.registration);
+router.post("/login", controller.login);
+router.get("/users", controller.getUsers);
+router.delete("/user:id", controller.deleteUser);
+
+module.exports = router;
